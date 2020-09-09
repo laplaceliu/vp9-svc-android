08321c41852643b8c5f047de58513e2803af44c0
laplaceliu <laplaceliu@outlook.com>
2020-09-09 09:21:25 +0800

    add ndk-build script

--- Android.mk
+++ Android.mk
@@ -1,1 +1,17 @@
-# Empty Android.mk to shadow libvpx/build/make/Android.mk
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+include libvpx/build/make/Android.mk
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := vp9_svc_encoder
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/libvpx
+
+LOCAL_SRC_FILES := vp9_svc_encoder.cpp
+
+LOCAL_SHARED_LIBRARIES := vpx
+
+include $(BUILD_SHARED_LIBRARY)
